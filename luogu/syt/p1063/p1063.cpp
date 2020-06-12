800e0f1f33575e207b260832a0013d9a102bbe5e
AndyShen2006 <andyshen2006@foxmail.com>
2020-06-12 20:57:55 +0800

    2020/20/57 Original Upgrade

---
+++ luogu/syt/p1063/p1063.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  //Installize
+  int n;
+  int z[200];
+  //Input
+  cin >> n;
+  for(int i=1; i<=n; i++){
+    cin >> z[i];
+  }
+  //Test input
+/*
+  cout << n << endl;
+  for(int i=1; i<=n; i++){
+    cout << z[i] << ' ';
+  }
+*/
+  //
+  return 0;
+}
